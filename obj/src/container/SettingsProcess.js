429f06bea07b67172f76b981ee60d90f242fc2c6
Dmitriy Krayniy <krdima92@gmail.com>
2018-08-20 13:35:37 +0300

    added DefaultRpcFactory

--- obj/src/container/SettingsProcess.js
+++ obj/src/container/SettingsProcess.js
@@ -2,10 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const pip_services_container_node_1 = require("pip-services-container-node");
 const SettingsServiceFactory_1 = require("../build/SettingsServiceFactory");
+const pip_services_rpc_node_1 = require("pip-services-rpc-node");
 class SettingsProcess extends pip_services_container_node_1.ProcessContainer {
     constructor() {
         super("settings", "Settings microservice");
         this._factories.add(new SettingsServiceFactory_1.SettingsServiceFactory);
+        this._factories.add(new pip_services_rpc_node_1.DefaultRpcFactory);
     }
 }
 exports.SettingsProcess = SettingsProcess;
--- src/container/SettingsProcess.ts
+++ src/container/SettingsProcess.ts
@@ -2,12 +2,14 @@ import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
 
 import { SettingsServiceFactory } from '../build/SettingsServiceFactory';
+import { DefaultRpcFactory } from 'pip-services-rpc-node';
 
 export class SettingsProcess extends ProcessContainer {
 
     public constructor() {
         super("settings", "Settings microservice");
         this._factories.add(new SettingsServiceFactory);
+        this._factories.add(new DefaultRpcFactory);
     }
 
 }
