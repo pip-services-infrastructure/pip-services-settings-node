79fe65a418bb811b08a035e5a82a220c91da0655
pipdeveloper <pipdevs@gmail.com>
2019-04-19 11:14:45 +0000

    project build by Travis CI [skip ci]

--- obj/src/logic/SettingsCommandSet.d.ts
+++ obj/src/logic/SettingsCommandSet.d.ts
@@ -3,9 +3,9 @@ import { ISettingsController } from './ISettingsController';
 export declare class SettingsCommandSet extends CommandSet {
     private _logic;
     constructor(logic: ISettingsController);
-    private makeGetSectionIdsCommand;
-    private makeGetSectionsCommand;
-    private makeGetSectionByIdCommand;
-    private makeSetSectionCommand;
-    private makeModifySectionCommand;
+    private makeGetSectionIdsCommand();
+    private makeGetSectionsCommand();
+    private makeGetSectionByIdCommand();
+    private makeSetSectionCommand();
+    private makeModifySectionCommand();
 }
