488e2cd1b8e97d8489f18c30d5fd00fbd57c071c
pipdeveloper <pipdevs@gmail.com>
2019-07-26 22:20:03 +0000

    project build by Travis CI [skip ci]

--- obj/src/services/version1/SettingsGrpcServiceV1.d.ts
+++ obj/src/services/version1/SettingsGrpcServiceV1.d.ts
@@ -4,10 +4,10 @@ export declare class SettingsGrpcServiceV1 extends GrpcService {
     private _controller;
     constructor();
     setReferences(references: IReferences): void;
-    private getSectionIds;
-    private getSections;
-    private getSectionById;
-    private setSection;
-    private modifySection;
+    private getSectionIds(call, callback);
+    private getSections(call, callback);
+    private getSectionById(call, callback);
+    private setSection(call, callback);
+    private modifySection(call, callback);
     register(): void;
 }
