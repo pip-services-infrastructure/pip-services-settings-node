c5ef701b2df78d683eefd5cd3bd82b831df78bf7
pipdeveloper <pipdevs@gmail.com>
2019-07-31 14:18:14 +0000

    project build by Travis CI [skip ci]

--- obj/src/persistence/SettingsMemoryPersistence.d.ts
+++ obj/src/persistence/SettingsMemoryPersistence.d.ts
@@ -7,8 +7,8 @@ import { SettingsSectionV1 } from '../data/version1/SettingsSectionV1';
 import { ISettingsPersistence } from './ISettingsPersistence';
 export declare class SettingsMemoryPersistence extends IdentifiableMemoryPersistence<SettingsSectionV1, string> implements ISettingsPersistence {
     constructor();
-    private matchString;
-    private composeFilter;
+    private matchString(value, search);
+    private composeFilter(filter);
     getPageByFilter(correlationId: string, filter: FilterParams, paging: PagingParams, callback: (err: any, page: DataPage<SettingsSectionV1>) => void): void;
     set(correlationId: string, item: SettingsSectionV1, callback?: (err: any, item: SettingsSectionV1) => void): void;
     modify(correlationId: string, id: string, updateParams: ConfigParams, incrementParams: ConfigParams, callback: (err: any, item: SettingsSectionV1) => void): void;
