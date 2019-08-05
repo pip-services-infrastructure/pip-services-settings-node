563a2805ffd63c1ece90047fb806b0f83ca597a0
pipdeveloper <pipdevs@gmail.com>
2019-08-05 17:07:13 +0000

    project build by Travis CI [skip ci]

--- obj/src/persistence/SettingsCouchbasePersistence.d.ts
+++ obj/src/persistence/SettingsCouchbasePersistence.d.ts
@@ -7,12 +7,12 @@ import { SettingsSectionV1 } from '../data/version1/SettingsSectionV1';
 import { ISettingsPersistence } from './ISettingsPersistence';
 export declare class SettingsCouchbasePersistence extends IdentifiableCouchbasePersistence<SettingsSectionV1, string> implements ISettingsPersistence {
     constructor();
-    private static mapToPublic;
-    private static fieldFromPublic;
-    private static mapFromPublic;
+    private static mapToPublic(map);
+    private static fieldFromPublic(field);
+    private static mapFromPublic(map);
     protected convertToPublic(value: any): any;
     protected convertFromPublic(value: any): any;
-    private composeFilter;
+    private composeFilter(filter);
     getPageByFilter(correlationId: string, filter: FilterParams, paging: PagingParams, callback: (err: any, page: DataPage<SettingsSectionV1>) => void): void;
     set(correlationId: string, item: SettingsSectionV1, callback?: (err: any, item: SettingsSectionV1) => void): void;
     modify(correlationId: string, id: string, updateParams: ConfigParams, incrementParams: ConfigParams, callback: (err: any, item: SettingsSectionV1) => void): void;
